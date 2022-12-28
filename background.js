4e20a29019443a821254d3a72b9adb7fd3658521
Harxxki <haruki4dev@gmail.com>
2022-12-29 01:58:59 +0900

    Save as Base64

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 const getUrl = () => {
   const elements = document.querySelectorAll('span.sc-artwork.sc-artwork-40x')
-  const unsecuredCopyToClipboard = (text) => {
+  const copyText = (text) => {
     const textArea = document.createElement('textarea')
     textArea.value = text
     document.body.prepend(textArea)
@@ -13,14 +13,28 @@ const getUrl = () => {
     }
     document.body.removeChild(textArea)
   }
+  const getImage = (imageUrl) => {
+    fetch(imageUrl)
+      .then((response) => response.blob()) // 画像のデータをblobとして取得する
+      .then((blob) => {
+        // blobをData URL形式に変換する
+        const reader = new FileReader()
+        reader.readAsDataURL(blob)
+        reader.onloadend = function () {
+          // Data URLをクリップボードに書き込む
+          navigator.clipboard.writeText(reader.result)
+        }
+      })
+  }
 
   for (const element of elements) {
     const style = element.style
     const backgroundImage = style.backgroundImage
     if (backgroundImage && backgroundImage.startsWith('url(')) {
-      const url = backgroundImage.substring(4, backgroundImage.length - 1)
+      const url = backgroundImage.substring(5, backgroundImage.length - 2)
       console.log(url)
-      unsecuredCopyToClipboard(url)
+      copyText(url)
+      getImage(url)
     }
   }
 }
